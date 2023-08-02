3193d13add30104855dcc00277a2a2ba57d9e97c
Nathan de Graaf <nathan@highbiza.nl>
2023-08-02 15:18:32 +0200

    die van mij ook niet

--- server/apps/dashboard/validators.py
+++ server/apps/dashboard/validators.py
@@ -3,10 +3,9 @@ from django.core.exceptions import ValidationError
 
 class FinalDestinationValidationMixin:
     def clean(self):
-        super().clean()
-
         if self.final and self.destinations.exists():
             raise ValidationError(
                 "Verwijder eerst de Bestemming(en) voordat je het vinkje 'final' aanvinkt."
             )
-        return
+
+        return super().clean()
