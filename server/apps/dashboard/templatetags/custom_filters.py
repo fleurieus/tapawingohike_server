108f03769c49d7a406453b79d921636c4e21a9e2
Vincent Fleur <v.w.fleur@saxion.nl>
2023-09-29 10:24:47 +0200

    Stats team time with seconds

--- server/apps/dashboard/templatetags/custom_filters.py
+++ server/apps/dashboard/templatetags/custom_filters.py
@@ -8,10 +8,20 @@ def get_item(dictionary, key):
     return dictionary.get(key)
 
 
+# @register.filter
+# def format_duration(duration):
+#     if duration is None or duration == timedelta(hours=0, minutes=0):
+#         return ''
+#     hours, remainder = divmod(duration.seconds, 3600)
+#     minutes, _ = divmod(remainder, 60)
+#     return f"{hours}:{minutes}"
 @register.filter
 def format_duration(duration):
     if duration is None or duration == timedelta(hours=0, minutes=0):
         return ''
+    
     hours, remainder = divmod(duration.seconds, 3600)
-    minutes, _ = divmod(remainder, 60)
-    return f"{hours}:{minutes}"
+    minutes, seconds = divmod(remainder, 60)
+    
+    # Use f-strings for Python 3.6+
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
